02e607357313c36b58caeeb4d2d7582c574d77ce
Jonas <gaopinsong@live.cn>
2022-08-05 11:49:31 +0800

    A simple netty server

--- server/netty/whatsuip/src/main/java/org/example/Application.java
+++ server/netty/whatsuip/src/main/java/org/example/Application.java
@@ -1,7 +1,67 @@
 package org.example;
 
-public class Application {
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.ReferenceCountUtil;
+
+public class Application extends ChannelInboundHandlerAdapter {
+
+    private final int port;
+
+    public Application(int port) {
+        this.port = port;
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        try {
+            System.out.println(msg);
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int port = 8080;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        }
+
+        try {
+            new Application(port).run();
+        } catch (InterruptedException e) {
+            System.out.println("Exit.");
+        }
+    }
+
+    private void run() throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new Application(port));
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, false);
+            ChannelFuture future = b.bind(port).sync();
+            future.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
     }
 }
